9522a8c5efa41cd5b0781c2a963b7e2e83514cde
Shishik Julia <oiseua@gmail.com>
2017-06-10 19:50:07 +0300

    les-relations-entre-entites-avec-doctrine2

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -3,10 +3,15 @@
 
 namespace OC\PlatformBundle\Controller;
 
+// N'oubliez pas ces use
+use OC\PlatformBundle\Entity\AdvertSkill;
+use OC\PlatformBundle\Entity\Advert;
+use OC\PlatformBundle\Entity\Image;
+use OC\PlatformBundle\Entity\Application;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 //use Symfony\Component\HttpFoundation\RedirectResponse; // N'oubliez pas ce use
-use Symfony\Component\HttpFoundation\Response;
+//use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 
@@ -62,7 +67,8 @@ class AdvertController extends Controller
        if ($page < 1) {
             // On déclenche une exception NotFoundHttpException, cela va afficher
             // une page d'erreur 404 (qu'on pourra personnaliser plus tard d'ailleurs)
-            throw new NotFoundHttpException('Page "'.$page.'" inexistante.');
+           throw new NotFoundHttpException
+           ('Page "'.$page.'" inexistante.');
         }
 
         // Ici, on récupérera la liste des annonces, puis on la passera au template
@@ -70,7 +76,7 @@ class AdvertController extends Controller
  //https://openclassrooms.com/courses/developpez-votre-site-web-avec-le-framework-symfony/les-services-theorie-et-creation-1
         // On a donc accès au conteneur :
 
-        $mailer = $this->container->get('mailer');
+      //  $mailer = $this->container->get('mailer');
 
         // On peut envoyer des e-mails, etc.
 
@@ -103,7 +109,8 @@ class AdvertController extends Controller
         );
 
         // Et modifiez le 2nd argument pour injecter notre liste
-        return $this->render('OCPlatformBundle:Advert:index.html.twig', array(
+        return $this->render
+        ('OCPlatformBundle:Advert:index.html.twig', array(
             'listAdverts' => $listAdverts
         ));
 
@@ -145,11 +152,8 @@ class AdvertController extends Controller
 
            // return new RedirectResponse($url);
 
-            /*
-             *
-             *
             // On crée la réponse sans lui donner de contenu pour le moment
-            $response = new Response();
+       /*     $response = new Response();
 
             // On définit le contenu
             $response->setContent("Ceci est une page d'erreur 404");
@@ -158,37 +162,45 @@ class AdvertController extends Controller
             $response->setStatusCode(Response::HTTP_NOT_FOUND);
 
             // On retourne la réponse
-            return $response;
-             }*/
+         //   return $response;
+           //  }
 
-        /*    return $this->render('OCPlatformBundle:Advert:view.html.twig', array(
+           return $this->render('OCPlatformBundle:Advert:view.html.twig', array(
                 'id' => $id
-            ));
-        }
-*/
+            ));*/
+   //     }
+
             // Ici, on récupérera l'annonce correspondante à l'id $id
 // On récupère le repository
-            $repository = $this->getDoctrine()
-                ->getManager()
-                ->getRepository('OCPlatformBundle:Advert')
+            $em = $this->getDoctrine()->getManager();
+
+               // On récupère l'annonce $id
+    $advert = $em->getRepository('OCPlatformBundle:Advert')
+                  ->find($id);
+
+    if (null === $advert) {
+        throw new NotFoundHttpException("L'annonce d'id ".$id." n'existe pas.");
+    }
+
+    // On récupère la liste des candidatures de cette annonce
+    $listApplications = $em
+        ->getRepository('OCPlatformBundle:Application')
+        ->findBy(array('advert' => $advert))
+    ;
+    //https://openclassrooms.com/courses/developpez-votre-site-web-avec-le-framework-symfony/les-relations-entre-entites-avec-doctrine2-1
+
+            // On récupère maintenant la liste des AdvertSkill
+            $listAdvertSkills = $em
+                ->getRepository('OCPlatformBundle:AdvertSkill')
+                ->findBy(array('advert' => $advert))
             ;
 
-            // On récupère l'entité correspondante à l'id $id
-            $advert = $repository->find($id);
-
-            // $advert est donc une instance de OC\PlatformBundle\Entity\Advert
-            // ou null si l'id $id  n'existe pas, d'où ce if :
-            if (null === $advert) {
-                throw new NotFoundHttpException("L'annonce d'id ".$id." n'existe pas.");
-            }
-
-            // Le render ne change pas, on passait avant un tableau, maintenant un objet
             return $this->render('OCPlatformBundle:Advert:view.html.twig', array(
-                'advert' => $advert
+                'advert'           => $advert,
+                'listApplications' => $listApplications,
+                'listAdvertSkills' => $listAdvertSkills
             ));
-
         }
-
 
 
     // Ajoutez cette méthode :
@@ -213,7 +225,7 @@ class AdvertController extends Controller
 // La gestion d'un formulaire est particulière, mais l'idée est la suivante :
 
        // Si la requête est en POST, c'est que le visiteur a soumis le formulaire
-       if ($request->isMethod('POST')) {
+  /*     if ($request->isMethod('POST')) {
            // Ici, on s'occupera de la création et de la gestion du formulaire
 
            $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
@@ -225,20 +237,22 @@ class AdvertController extends Controller
        // Si on n'est pas en POST, alors on affiche le formulaire
        return $this->render('OCPlatformBundle:Advert:add.html.twig');
 
-
+*/
        //https://openclassrooms.com/courses/developpez-votre-site-web-avec-le-framework-symfony/les-services-theorie-et-creation-1
        // On récupère le service
-       $antispam = $this->container->get('oc_platform.antispam');
+     /*  $antispam = $this->container->get('oc_platform.antispam');
 
        // Je pars du principe que $text contient le texte d'un message quelconque
        $text = '...';
        if ($antispam->isSpam($text)) {
            throw new \Exception('Votre message a été détecté comme spam !');
        }
+     */
 
        // Ici le message n'est pas un spam
 
-
+// On récupère l'EntityManager
+       $em = $this->getDoctrine()->getManager();
 
 //https://openclassrooms.com/courses/developpez-votre-site-web-avec-le-framework-symfony/manipuler-ses-entites-avec-doctrine2-1
        // Création de l'entité
@@ -249,32 +263,113 @@ class AdvertController extends Controller
        // On peut ne pas définir ni la date ni la publication,
        // car ces attributs sont définis automatiquement dans le constructeur
 
+//https://openclassrooms.com/courses/developpez-votre-site-web-avec-le-framework-symfony/les-relations-entre-entites-avec-doctrine2-1
+       // Création de l'entité Image
+       $image = new Image();
+       $image->setUrl('http://sdz-upload.s3.amazonaws.com/prod/upload/job-de-reve.jpg');
+       $image->setAlt('Job de rêve');
+
+       // On lie l'image à l'annonce
+       $advert->setImage($image);
+
+       // Création d'une première candidature
+       $application1 = new Application();
+       $application1->setAuthor('Marine');
+       $application1->setContent("J'ai toutes les qualités requises.");
+
+       // Création d'une deuxième candidature par exemple
+       $application2 = new Application();
+       $application2->setAuthor('Pierre');
+       $application2->setContent("Je suis très motivé.");
+
+       // On lie les candidatures à l'annonce
+       $application1->setAdvert($advert);
+       $application2->setAdvert($advert);
+
+ //https://openclassrooms.com/courses/developpez-votre-site-web-avec-le-framework-symfony/les-relations-entre-entites-avec-doctrine2-1
+// On récupère toutes les compétences possibles
+       $listSkills = $em->getRepository('OCPlatformBundle:Skill')
+           ->findAll();
+
+       // Pour chaque compétence
+       foreach ($listSkills as $skill) {
+           // On crée une nouvelle « relation entre 1 annonce et 1 compétence »
+           $advertSkill = new AdvertSkill();
+
+           // On la lie à l'annonce, qui est ici toujours la même
+           $advertSkill->setAdvert($advert);
+           // On la lie à la compétence, qui change ici dans la boucle foreach
+           $advertSkill->setSkill($skill);
+
+           // Arbitrairement, on dit que chaque compétence est requise au niveau 'Expert'
+           $advertSkill->setLevel('Expert');
+
+           // Et bien sûr, on persiste cette entité de relation, propriétaire des deux autres relations
+           $em->persist($advertSkill);
+       }
+
+       // Doctrine ne connait pas encore l'entité $advert. Si vous n'avez pas défini la relation AdvertSkill
+
        // On récupère l'EntityManager
-       $em = $this->getDoctrine()->getManager();
+     //  $em = $this->getDoctrine()->getManager();
 
        // Étape 1 : On « persiste » l'entité
        $em->persist($advert);
 
-       // Étape 2 : On « flush » tout ce qui a été persisté avant
+       // Étape 1 bis : si on n'avait pas défini le cascade={"persist"},
+       // on devrait persister à la main l'entité $image
+       // $em->persist($image);
+
+       // Étape 1 ter : pour cette relation pas de cascade lorsqu'on persiste Advert, car la relation est
+       // définie dans l'entité Application et non Advert. On doit donc tout persister à la main ici.
+       $em->persist($application1);
+       $em->persist($application2);
+
+       // Étape 2 : On déclenche l'enregistrement
        $em->flush();
 
        // Reste de la méthode qu'on avait déjà écrit
        if ($request->isMethod('POST')) {
-           $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
+           $request->getSession()->getFlashBag()
+               ->add('notice', 'Annonce bien enregistrée.');
 
            // Puis on redirige vers la page de visualisation de cettte annonce
-           return $this->redirectToRoute('oc_platform_view', array('id' => $advert->getId()));
+           return $this->redirectToRoute('oc_platform_view',
+               array('id' => $advert->getId()));
        }
 
        // Si on n'est pas en POST, alors on affiche le formulaire
-       return $this->render('OCPlatformBundle:Advert:add.html.twig', array('advert' => $advert));
-
+       return $this->render('OCPlatformBundle:Advert:add.html.twig');
 
    }
 
 
     public function editAction($id, Request $request)
     {
+ //https://openclassrooms.com/courses/developpez-votre-site-web-avec-le-framework-symfony/les-relations-entre-entites-avec-doctrine2-1
+        $em = $this->getDoctrine()->getManager();
+
+        // On récupère l'annonce $id
+        $advert = $em->getRepository('OCPlatformBundle:Advert')->find($id);
+
+        if (null === $advert) {
+            throw new NotFoundHttpException("L'annonce d'id ".$id." n'existe pas.");
+        }
+
+        // La méthode findAll retourne toutes les catégories de la base de données
+        $listCategories = $em->getRepository('OCPlatformBundle:Category')->findAll();
+
+        // On boucle sur les catégories pour les lier à l'annonce
+        foreach ($listCategories as $category) {
+            $advert->addCategory($category);
+        }
+
+        // Pour persister le changement dans la relation, il faut persister l'entité propriétaire
+        // Ici, Advert est le propriétaire, donc inutile de la persister car on l'a récupérée depuis Doctrine
+
+        // Étape 2 : On déclenche l'enregistrement
+        $em->flush();
+
         // Ici, on récupérera l'annonce correspondante à $id
 
         // Même mécanisme que pour l'ajout
@@ -284,21 +379,44 @@ class AdvertController extends Controller
             return $this->redirectToRoute('oc_platform_view', array('id' => 5));
         }
 
-        $advert = array(
+     /*   $advert = array(
             'title' => 'Recherche développpeur Symfony',
             'id' => $id,
             'author' => 'Alexandre',
             'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
             'date' => new \Datetime()
-        );
+        );*/
 
-        return $this->render('OCPlatformBundle:Advert:edit.html.twig', array(
+        return $this->render('OCPlatformBundle:Advert:
+        edit.html.twig', array(
             'advert' => $advert
         ));
     }
 
     public function deleteAction($id)
     {
+ //https://openclassrooms.com/courses/developpez-votre-site-web-avec-le-framework-symfony/les-relations-entre-entites-avec-doctrine2-1
+        $em = $this->getDoctrine()->getManager();
+
+        // On récupère l'annonce $id
+        $advert = $em->getRepository('OCPlatformBundle:Advert')
+            ->find($id);
+
+        if (null === $advert) {
+            throw new NotFoundHttpException("L'annonce d'id ".$id." n'existe pas.");
+        }
+
+        // On boucle sur les catégories de l'annonce pour les supprimer
+        foreach ($advert->getCategories() as $category) {
+            $advert->removeCategory($category);
+        }
+
+        // Pour persister le changement dans la relation, il faut persister l'entité propriétaire
+        // Ici, Advert est le propriétaire, donc inutile de la persister car on l'a récupérée depuis Doctrine
+
+        // On déclenche la modification
+        $em->flush();
+
         // Ici, on récupérera l'annonce correspondant à $id
 
         // Ici, on gérera la suppression de l'annonce en question
@@ -309,7 +427,7 @@ class AdvertController extends Controller
     {
         // On fixe en dur une liste ici, bien entendu par la suite
         // on la récupérera depuis la BDD !
-        $listAdverts = array(
+       $listAdverts = array(
             array('id' => 2, 'title' => 'Recherche développeur Symfony'),
             array('id' => 5, 'title' => 'Mission de webmaster'),
             array('id' => 9, 'title' => 'Offre de stage webdesigner')
--- src/OC/PlatformBundle/Entity/Advert.php
+++ src/OC/PlatformBundle/Entity/Advert.php
@@ -1,13 +1,10 @@
 <?php
 
 namespace OC\PlatformBundle\Entity;
-
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
-
 /**
- * Advert
- *
- * @ORM\Table(name="advert")
+ * @ORM\Table(name="oc_advert")
  * @ORM\Entity(repositoryClass="OC\PlatformBundle\Repository\AdvertRepository")
  */
 class Advert
@@ -19,146 +16,111 @@ class Advert
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
      */
-
     private $id;
-
     /**
      * @var \DateTime
      *
-     * @ORM\Column(name="date", type="datetime", length=255)
+     * @ORM\Column(name="date", type="datetime")
      */
     private $date;
-
     /**
      * @var string
      *
      * @ORM\Column(name="title", type="string", length=255)
      */
     private $title;
-
     /**
      * @var string
      *
      * @ORM\Column(name="author", type="string", length=255)
      */
     private $author;
-
     /**
      * @var string
      *
-     * @ORM\Column(name="content", type="text")
+     * @ORM\Column(name="content", type="string", length=255)
      */
     private $content;
     /**
      * @ORM\Column(name="published", type="boolean")
      */
     private $published = true;
+    /**
+     * @ORM\OneToOne(targetEntity="OC\PlatformBundle\Entity\Image", cascade={"persist"})
+     */
+    private $image;
+    /**
+     * @ORM\ManyToMany(targetEntity="OC\PlatformBundle\Entity\Category", cascade={"persist"})
+     * @ORM\JoinTable(name="oc_advert_category")
+     */
+    private $categories;
+    /**
+     * @ORM\OneToMany(targetEntity="OC\PlatformBundle\Entity\Application", mappedBy="advert")
+     */
+    private $applications; // Notez le « s », une annonce est liée à plusieurs candidatures
     public function __construct()
     {
-        // Par défaut, la date de l'annonce est la date d'aujourd'hui
-        $this->date = new \Datetime();
+        $this->date         = new \Datetime();
+        $this->categories   = new ArrayCollection();
+        $this->applications = new ArrayCollection();
     }
-
-
     /**
-     * Get id
-     *
      * @return int
      */
     public function getId()
     {
         return $this->id;
     }
-
     /**
-     * Set date
-     *
-     * @param string $date
-     *
-     * @return Advert
+     * @param \DateTime $date
      */
     public function setDate($date)
     {
         $this->date = $date;
-
-        return $this;
     }
-
     /**
-     * Get date
-     *
-     * @return string
+     * @return \DateTime
      */
     public function getDate()
     {
         return $this->date;
     }
-
     /**
-     * Set title
-     *
      * @param string $title
-     *
-     * @return Advert
      */
     public function setTitle($title)
     {
         $this->title = $title;
-
-        return $this;
     }
-
     /**
-     * Get title
-     *
      * @return string
      */
     public function getTitle()
     {
         return $this->title;
     }
-
     /**
-     * Set author
-     *
      * @param string $author
-     *
-     * @return Advert
      */
     public function setAuthor($author)
     {
         $this->author = $author;
-
-        return $this;
     }
-
     /**
-     * Get author
-     *
      * @return string
      */
     public function getAuthor()
     {
         return $this->author;
     }
-
     /**
-     * Set content
-     *
      * @param string $content
-     *
-     * @return Advert
      */
     public function setContent($content)
     {
         $this->content = $content;
-
-        return $this;
     }
-
     /**
-     * Get content
-     *
      * @return string
      */
     public function getContent()
@@ -179,6 +141,56 @@ class Advert
     {
         return $this->published;
     }
-
+    public function setImage(Image $image = null)
+    {
+        $this->image = $image;
+    }
+    public function getImage()
+    {
+        return $this->image;
+    }
+    /**
+     * @param Category $category
+     */
+    public function addCategory(Category $category)
+    {
+        $this->categories[] = $category;
+    }
+    /**
+     * @param Category $category
+     */
+    public function removeCategory(Category $category)
+    {
+        $this->categories->removeElement($category);
+    }
+    /**
+     * @return ArrayCollection
+     */
+    public function getCategories()
+    {
+        return $this->categories;
+    }
+    /**
+     * @param Application $application
+     */
+    public function addApplication(Application $application)
+    {
+        $this->applications[] = $application;
+        // On lie l'annonce à la candidature
+        $application->setAdvert($this);
+    }
+    /**
+     * @param Application $application
+     */
+    public function removeApplication(Application $application)
+    {
+        $this->applications->removeElement($application);
+    }
+    /**
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getApplications()
+    {
+        return $this->applications;
+    }
 }
-
